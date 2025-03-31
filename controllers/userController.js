77813a0e2c17f1933cec7982c427cacab7d3e55e
tajmannkaur <tajmannkamboj08@gmail.com>
2025-03-31 10:43:06 -0600

    update

---
+++ controllers/userController.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+const filePath = './database.json';
+const { readData, writeData } = require('./utils/file.js');
+
+
+
+async function createUser(req, res){
+    try {
+        //function is a utility class function 
+        const data = await readData();
+
+        
+    } catch (error) {
+        
+    }
+
+
+    
+}
---
+++ utils/file.js
@@ -0,0 +1,24 @@
+const fs = require('fs').promises;
+const filePath = require('./database.json');
+
+
+async function readData(){
+    try{
+        const data = await fs.readFile(filePath, 'utf-8');
+        return JSON.parse(data);
+    }catch(error){
+    throw new Error('Internal Server Error');
+    
+    }
+    }
+
+async function writeData(data){
+try{
+    const data = await fs.readFile(filePath,JSON.stringify(data, null, 2));
+}catch(error){
+throw new Error('Internal Server Error');
+
+}
+
+}
+module.exports = {readData, writeData};
